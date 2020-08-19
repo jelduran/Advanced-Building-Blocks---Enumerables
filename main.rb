d448f91b666529cf640a61e6599fbf46db45f137
John Elvis Durán <jelduran@gmail.com>
2020-08-19 15:25:02 -0500

    my_each statement:
    This instruction is to iterate through the object if it is an Hash, using the length of the object as an argument for times method, executing a block with index as argument and using yield method for a next block, using the keys variable to get the key and the value of this Hash item and pass it like an Array object.

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ module Enumerable
     keys = self.keys if self.is_a?(Hash)
     if block_given?
       self.length.times {|index| yield(self[index])} if self.is_a?(Array)
+      self.length.times {|index| yield(keys[index],self[keys[index]])} if self.is_a?(Hash)
     end
   end
 end
