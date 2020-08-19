b0ea2dcb036200bc403a860a1859c321c05285fc
Carlos Robles <carlos.ssh@icloud.com>
2020-08-19 15:39:04 -0500

    my_each_with_index method:
    This method is basically a clone of my_each method the only difference is it also send the value of the index to the next block.

--- main.rb
+++ main.rb
@@ -9,4 +9,16 @@ module Enumerable
       self.to_enum(:my_each)
     end
   end
+
+  def my_each_with_index
+    keys = self.keys if self.is_a?(Hash)
+    if block_given?
+      self.length.times {|index| yield(self[index],index)} if self.is_a?(Array)
+      self.length.times {|index| yield([keys[index],self[keys[index]]],index)} if self.is_a?(Hash)
+      self
+    else
+      self.to_enum(:my_each)
+    end
+  end
+
 end
