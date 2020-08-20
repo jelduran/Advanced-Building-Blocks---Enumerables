ae6c6c58fd86387234e540576b7e404c9b452731
John Elvis Durán <jelduran@gmail.com>
2020-08-20 02:10:56 -0500

    Rubocop 1:
    Fixed coding problems about bad spacing, perceived complexity, bad use of literal assignments and bad use of triple equal operator.

--- main.rb
+++ main.rb
@@ -1,109 +1,107 @@
 module Enumerable
   def my_each
-    keys = self.keys if self.is_a?(Hash)
+    keys = self.keys if is_a?(Hash)
     if block_given?
-      self.length.times {|index| yield(self[index])} if self.is_a?(Array)
-      self.length.times {|index| yield(keys[index],self[keys[index]])} if self.is_a?(Hash)
+      length.times { |index| yield(self[index]) } if is_a?(Array)
+      length.times { |index| yield(keys[index], self[keys[index]]) } if is_a?(Hash)
       self
-    else
-      self.to_enum(:my_each)
     end
+    to_enum(:my_each)
   end
 
   def my_each_with_index
-    keys = self.keys if self.is_a?(Hash)
+    keys = self.keys if is_a?(Hash)
     if block_given?
-      self.length.times {|index| yield(self[index],index)} if self.is_a?(Array)
-      self.length.times {|index| yield([keys[index],self[keys[index]]],index)} if self.is_a?(Hash)
+      length.times { |index| yield(self[index], index) } if is_a?(Array)
+      length.times { |index| yield([keys[index], self[keys[index]]], index) } if is_a?(Hash)
       self
-    else
-      self.to_enum(:my_each)
     end
+    to_enum(:my_each_with_index)
   end
 
   def my_select
-    self.is_a?(Hash) ? new_enumerable = {} : new_enumerable = []
+    new_enumerable = empty_enum
     if block_given?
-      self.my_each {|item| new_enumerable.push(item) if yield(item)} if self.is_a?(Array)
-      self.my_each {|key,value| new_enumerable[key]=value if yield(key,value)} if self.is_a?(Hash)
+      my_each { |item| new_enumerable.push(item) if yield(item) } if is_a?(Array)
+      my_each { |key, value| new_enumerable[key] = value if yield(key, value) } if is_a?(Hash)
       new_enumerable
-    else
-      self.to_enum(:my_select)
     end
+    to_enum(:my_select)
   end
 
-  def my_all? pattern=nil
+  def my_all?(pattern = nil)
     if block_given?
-      self.my_each {|item| return false unless yield item} if self.is_a?(Array)
-      self.my_each {|key,value| return false unless yield [key,value]} if self.is_a?(Hash)
+      my_each { |item| return false unless yield item } if is_a?(Array)
+      my_each { |key, value| return false unless yield [key, value] } if is_a?(Hash)
       true
-    elsif pattern
-      self.my_all? {|item| pattern===item}
-    else
-      self.my_all? {|item| item}
     end
+    my_all? { |item| item =~ pattern } if pattern.is_a?(Regexp)
+    my_all? { |item| item }
   end
 
-  def my_any? pattern=nil
+  def my_any?(pattern = nil)
     if block_given?
-      self.my_each {|item| return true if yield item} if self.is_a?(Array)
-      self.my_each {|key,value| return true if yield [key,value]} if self.is_a?(Hash)
+      my_each { |item| return true if yield item } if is_a?(Array)
+      my_each { |key, value| return true if yield [key, value] } if is_a?(Hash)
       false
-    elsif pattern
-      self.my_any? {|item| pattern===item}
-    else
-      self.my_any? {|item| item}
     end
+    my_any? { |item| item =~ pattern } if pattern.is_a?(Regexp)
+    my_any? { |item| item }
   end
 
-  def my_none? pattern=nil
+  def my_none?(pattern = nil)
     if block_given?
-      self.my_each {|item| return false if yield item} if self.is_a?(Array)
-      self.my_each {|key,value| return false if yield [key,value]} if self.is_a?(Hash)
+      my_each { |item| return false if yield item } if is_a?(Array)
+      my_each { |key, value| return false if yield [key, value] } if is_a?(Hash)
       true
-    elsif pattern
-      self.my_none? {|item| pattern===item}
-    else
-      self.my_none? {|item| item}
     end
+    my_none? { |item| item =~ pattern } if pattern.is_a?(Regexp)
+    my_none? { |item| item }
   end
 
-  def my_count search=nil
+  def my_count(search = nil)
     counter = 0
     if block_given?
-      self.my_each {|item| counter+=1 if yield(item)} if self.is_a?(Array)
-      self.my_each {|key,value| counter+=1 if yield([key,value])} if self.is_a?(Hash)
+      my_each { |item| counter += 1 if yield(item) } if is_a?(Array)
+      my_each { |key, value| counter += 1 if yield([key, value]) } if is_a?(Hash)
       counter
-    elsif search
-      self.my_count {|item| item==search}
-    else
-      self.length
     end
+    my_count { |item| item == search } if search
+    length
   end
 
-  def my_map proc=nil
+  def my_map(proc = nil)
     map = []
     if proc.is_a?(Proc)
-      self.my_each {|item| map << proc.call(item)} if self.is_a?(Array)
-      self.my_each {|key,value| map << proc.call([key,value])} if self.is_a?(Hash)
+      my_each { |item| map << proc.call(item) } if is_a?(Array)
+      my_each { |key, value| map << proc.call([key, value]) } if is_a?(Hash)
       map
-    elsif block_given?
-      self.my_each {|item| map << yield(item)} if self.is_a?(Array)
-      self.my_each {|key,value| map << yield([key,value])} if self.is_a?(Hash)
-      map
-    else
-      self.to_enum(:my_map)
     end
+    if block_given?
+      my_each { |item| map << yield(item) } if is_a?(Array)
+      my_each { |key, value| map << yield([key, value]) } if is_a?(Hash)
+      map
+    end
+    to_enum(:my_map)
   end
 
-  def my_inject(memo=nil,&sym)
-    self.my_each {|item| memo.nil? ? memo=item : memo=yield(memo,item)} if self.is_a?(Array)
-    self.my_each {|key,value| memo.nil? ? memo=[key,value] : memo=yield(memo,[key,value])} if self.is_a?(Hash)
+  def my_inject(memo = nil, &_sym)
+    my_each { |item| memo = item ? memo.nil? : memo = yield(memo, item) } if is_a?(Array)
+    if is_a?(Hash)
+      my_each do |key, value|
+        item = [key, value]
+        memo = item ? memo.nil? : memo = yield(memo, [key, value])
+      end
+    end
     memo
   end
 
-  def multiply_els
-    self.my_inject(&:*)
+  def empty_enum
+    empty_hash = {}
+    empty_hash ? is_a?(Hash) : []
   end
 
+  def multiply_els
+    my_inject(&:*)
+  end
 end
