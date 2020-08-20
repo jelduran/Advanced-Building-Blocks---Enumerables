6e79a4005fd4272a2454278e3d2465b7699fc8e0
John Elvis Durán <jelduran@gmail.com>
2020-08-20 08:06:53 -0500

    Rubocop 2:
    Fixed coding problems about cyclomatic complexity.

--- main.rb
+++ main.rb
@@ -20,51 +20,68 @@ module Enumerable
   end
 
   def my_select
-    new_enumerable = empty_enum
+    new_enumerable = []
     if block_given?
-      my_each { |item| new_enumerable.push(item) if yield(item) } if is_a?(Array)
+      is_a?(Array) ? my_each { |item| new_enumerable.push(item) if yield(item) } : new_enumerable = {}
       my_each { |key, value| new_enumerable[key] = value if yield(key, value) } if is_a?(Hash)
-      new_enumerable
+      return new_enumerable
     end
     to_enum(:my_select)
   end
 
   def my_all?(pattern = nil)
     if block_given?
-      my_each { |item| return false unless yield item } if is_a?(Array)
-      my_each { |key, value| return false unless yield [key, value] } if is_a?(Hash)
-      true
+      my_each_with_index { |item| return false unless yield item }
+      return true
     end
-    my_all? { |item| item =~ pattern } if pattern.is_a?(Regexp)
-    my_all? { |item| item }
+    if pattern
+      case pattern.class.to_s
+      when 'Class' then my_all? { |item| item.is_a?(pattern) }
+      when 'Regexp' then my_all? { |item| item =~ pattern }
+      else false
+      end
+    else
+      my_all? { |item| item }
+    end
   end
 
   def my_any?(pattern = nil)
     if block_given?
-      my_each { |item| return true if yield item } if is_a?(Array)
-      my_each { |key, value| return true if yield [key, value] } if is_a?(Hash)
-      false
+      my_each_with_index { |item| return true if yield item }
+      return false
     end
-    my_any? { |item| item =~ pattern } if pattern.is_a?(Regexp)
-    my_any? { |item| item }
+    if pattern
+      case pattern.class.to_s
+      when 'Class' then my_any? { |item| item.is_a?(pattern) }
+      when 'Regexp' then my_any? { |item| item =~ pattern }
+      else false
+      end
+    else
+      my_any? { |item| item }
+    end
   end
 
   def my_none?(pattern = nil)
     if block_given?
-      my_each { |item| return false if yield item } if is_a?(Array)
-      my_each { |key, value| return false if yield [key, value] } if is_a?(Hash)
-      true
+      my_each_with_index { |item| return false if yield item }
+      return true
     end
-    my_none? { |item| item =~ pattern } if pattern.is_a?(Regexp)
-    my_none? { |item| item }
+    if pattern
+      case pattern.class.to_s
+      when 'Class' then my_none? { |item| item.is_a?(pattern) }
+      when 'Regexp' then my_none? { |item| item =~ pattern }
+      else false
+      end
+    else
+      my_none? { |item| item }
+    end
   end
 
   def my_count(search = nil)
     counter = 0
     if block_given?
-      my_each { |item| counter += 1 if yield(item) } if is_a?(Array)
-      my_each { |key, value| counter += 1 if yield([key, value]) } if is_a?(Hash)
-      counter
+      my_each_with_index { |item| counter += 1 if yield(item) }
+      return counter
     end
     my_count { |item| item == search } if search
     length
@@ -73,32 +90,19 @@ module Enumerable
   def my_map(proc = nil)
     map = []
     if proc.is_a?(Proc)
-      my_each { |item| map << proc.call(item) } if is_a?(Array)
-      my_each { |key, value| map << proc.call([key, value]) } if is_a?(Hash)
-      map
+      my_each_with_index { |item| map << proc.call(item) }
+      return map
     end
     if block_given?
-      my_each { |item| map << yield(item) } if is_a?(Array)
-      my_each { |key, value| map << yield([key, value]) } if is_a?(Hash)
-      map
+      my_each_with_index { |item| map << yield(item) }
+      return map
     end
     to_enum(:my_map)
   end
 
   def my_inject(memo = nil, &_sym)
-    my_each { |item| memo = item ? memo.nil? : memo = yield(memo, item) } if is_a?(Array)
-    if is_a?(Hash)
-      my_each do |key, value|
-        item = [key, value]
-        memo = item ? memo.nil? : memo = yield(memo, [key, value])
-      end
-    end
+    my_each_with_index { |item| memo = memo.nil? ? item : yield(memo, item) }
     memo
-  end
-
-  def empty_enum
-    empty_hash = {}
-    empty_hash ? is_a?(Hash) : []
   end
 
   def multiply_els
