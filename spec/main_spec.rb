4aa4143b5cf19fbfa6d174006c16c3dd456e1f97
Yanick <tyankee15@gmail.com>
2020-09-18 17:30:45 -0300

    Test my_each method:
    created test to check if array is returned when block is given

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -8,5 +8,9 @@ describe Enumerable do
     it 'checks if returns an Enumerator object when no block is given.' do
       expect(arr.my_each).to(satisfy { |output| output.is_a?(Enumerator) })
     end
+
+    it 'checks if array is returned when block is given' do
+      expect(arr.my_each {|index| index}).to eql(arr)
+    end
   end
 end
