2a48658099c88daf437ed42fbe740ea1d04c30fb
John Elvis Durán <jelduran@gmail.com>
2020-09-18 15:48:16 -0500

    Test #my_each 3:
    Checks if method makes an action for every item in the enumerable.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,13 +4,19 @@ require_relative '../main'
 
 describe Enumerable do
   let(:arr) { [1, 2, 3, 4, 5] }
+  let(:target) { [] }
   describe '#my_each' do
     it 'checks if returns an Enumerator object when no block is given.' do
       expect(arr.my_each).to(satisfy { |output| output.is_a?(Enumerator) })
     end
 
     it 'checks if array is returned when block is given' do
-      expect(arr.my_each {|index| index}).to eql(arr)
+      expect(arr.my_each { |index| index }).to eql(arr)
+    end
+
+    it 'checks if method makes an action for every item in the enumerable.' do
+      arr.my_each { |i| target.push(i + 5) }
+      expect(target).to(satisfy { |t| t == [6, 7, 8, 9, 10] })
     end
   end
 end
